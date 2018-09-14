5755c890b8cf90a73fc66db51be891c6ecfca8b9
Paul Orlando <porlan3@gmail.com>
2018-09-13 23:51:14 -0400

    fix syntax on new core classes

--- src/main/java/com/example/helloworld/core/Converter.java
+++ src/main/java/com/example/helloworld/core/Converter.java
@@ -1,25 +1,28 @@
 package com.example.helloworld.core;
 import java.util.HashMap;
+import java.util.Map;
 
-public Class Converter throws Exception {
+public class Converter {
 	String inputType;
 	String direction;
 	double inputNumber;
-	static final Map<String, Float> map = new HashMap<>(3);
-	map.put('LBS_KG', 0.453592);
-	map.put('GAL_L', 3.78541);
-	map.put('MI_KM', 1.60934);
+	static Map<String, Float> map = new HashMap<String, Float>(3);
+	static {
+		map.put("LBS_KG", new Float(0.453592));
+		map.put("GAL_L", new Float(3.78541));
+		map.put("MI_KM", new Float(1.60934));
+	}
 
-	Converter(String queryString) {
+	Converter(String queryString) throws Exception {
 		QueryParser queryParser = new QueryParser(queryString);
 		queryParser.parse();
 		inputType = queryParser.inputType;
 		inputNumber = queryParser.inputNumber;
-		direction = (queryParser.inputType == 'lbs' || queryParser.inputType == 'gal' || queryParser.inputType == 'mi');
+		direction = (queryParser.inputType == "lbs" || queryParser.inputType == "gal" || queryParser.inputType == "mi")? "forward": "backward";
 	}
 
 	double convertInput() {
-		if (direction === 'forward') {
+		if (direction == "forward") {
 			return inputNumber*map.get(inputType);
 		}
 		return inputNumber/map.get(inputType);
--- src/main/java/com/example/helloworld/core/QueryParser.java
+++ src/main/java/com/example/helloworld/core/QueryParser.java
@@ -1,37 +1,36 @@
 package com.example.helloworld.core;
 
-public Class QueryParser throws Exception {
+public class QueryParser {
 	String queryString;
 	String inputType;
 	float inputNumber;
 
-	QueryParser(queryString) {
+	QueryParser(String queryString) {
 		this.queryString = queryString;
 	}
 
-	public void parse() {
-		String units;
-		float value;
+	public void parse() throws Exception {
 		String valueString;
-		for (int i = queryString.length()-1; i > -1; i--) {
-			if (queryString.charAt(i).isDigit()) {
+		int i;
+		for (i = queryString.length()-1; i > -1; i--) {
+			if (Character.isDigit(queryString.charAt(i))) {
 				break;
 			}
 		}
 		if (i == queryString.length()-1 || i == 0) {
-			throw new Exception('No Good Input!');
+			throw new Exception();
 		}
 		inputType = queryString.substring(i, queryString.length()-1).toString();
-		if (inputType != 'km' || inputType != 'mi' || inputType != 'gal' || inputType != 'l' || inputType != 'lbs' || inputType != 'kg') {
-			throw new Error('No Good Input!');
+		if (inputType != "km" || inputType != "mi" || inputType != "gal" || inputType != "l" || inputType != "lbs" || inputType != "kg") {
+			throw new Exception();
 		}
 
 		valueString = queryString.substring(0, i).toString().toLowerCase();
-		if (valueString.contains('/')) {
-			String[] split = valueString.split('/');
-			inputNumber = Float(valueString[0])/Float(valueString[1]);
+		if (valueString.contains("/")) {
+			String[] split = valueString.split("/");
+			inputNumber = Float.parseFloat(split[0])/Float.parseFloat(split[1]);
 		} else {
-			inputNumber = Float(valueString);
+			inputNumber = Float.parseFloat(valueString);
 		}
 	}
 }
